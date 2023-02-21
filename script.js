8541598a2f921a9527cd32a1e95d287b876f4bf7
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-02-21 23:34:36 +0700

    load dynamic book

--- script.js
+++ script.js
@@ -12,7 +12,30 @@ function addBookToLibary(book) {
 }
 
 function loadLibary() {
-  console.log(myLibrary);
+  const library = document.getElementById("library");
+
+  myLibrary.forEach((item) => {
+    const book = bookCreator(item);
+
+    library.insertAdjacentHTML("beforeend", book);
+  });
+}
+
+function bookCreator(bookData) {
+  const html = `
+    <div class="book">
+      <h1 class="book-title" class="book-title">Title : ${bookData.title}</h1>
+      <p>Author : ${bookData.author}</p>
+      <p>Number of Pages : ${bookData.numberOfPages}</p>
+      <p>Status : ${bookData.status}</p>
+      <div class="button-wrapper">
+        <button class="btn">Finished</button>
+        <button class="btn">Delete</button>
+      </div>
+    </div>
+  `;
+
+  return html;
 }
 
 const newBookOne = new Book(
