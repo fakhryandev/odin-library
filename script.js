07fb6ae135ae8ec57c682545242e044a2ad4c170
Muhammad Fakhryan <fakhryandev@gmail.com>
2024-01-03 14:59:22 +0700

    validate book form and reset form after modal close

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 const myLibrary = [];
-const bookForm = document.getElementById("newBookForm");
-const formModal = document.getElementById("modalNewBook");
+const bookForm = document.getElementById('newBookForm');
 
-document.addEventListener("DOMContentLoaded", () => {
-  loadLibary();
+(() => {
+  'use strict';
+
+  const forms = document.querySelectorAll('.needs-validation');
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+          form.classList.add('was-validated');
+        }
+
+        if (form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+
+          const author = document.getElementById('authorInput').value;
+          const title = document.getElementById('titleInput').value;
+          const numberOfPages = document.getElementById('pagesInput').value;
+          const status = document.getElementById('statusCheck').checked;
+
+          const newBook = new Book(title, author, numberOfPages, status);
+
+          bookForm.reset();
+          addBookToLibary(newBook);
+
+          $('#modalNewBook').modal('hide');
+          loadLibary();
+          form.classList.remove('was-validated');
+        }
+      },
+      false
+    );
+  });
+})();
+
+$('#modalNewBook').on('hidden.bs.modal', function () {
+  const bookForm = document.getElementById('newBookForm');
+  bookForm.reset();
+  bookForm.classList.remove('was-validated');
 });
 
-bookForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const author = document.getElementById("authorInput").value;
-  const title = document.getElementById("titleInput").value;
-  const numberOfPages = document.getElementById("pagesInput").value;
-  const status = document.getElementById("statusCheck").checked;
-
-  const newBook = new Book(title, author, numberOfPages, status);
-
-  bookForm.reset();
-  addBookToLibary(newBook);
-
-  $("#modalNewBook").modal("hide");
+document.addEventListener('DOMContentLoaded', () => {
   loadLibary();
 });
 
@@ -35,24 +62,24 @@ function addBookToLibary(book) {
 }
 
 function loadLibary() {
-  const library = document.getElementById("library");
+  const library = document.getElementById('library');
 
-  library.innerHTML = "";
+  library.innerHTML = '';
 
   myLibrary.forEach((item, index) => {
     const book = bookCreator(item, index);
 
-    library.insertAdjacentHTML("beforeend", book);
+    library.insertAdjacentHTML('beforeend', book);
 
     const removeButton = document.getElementById(`remove-${index}`);
 
-    removeButton.addEventListener("click", (e) => {
+    removeButton.addEventListener('click', (e) => {
       myLibrary.splice(index, 1);
       loadLibary();
     });
 
     const updateButton = document.getElementById(`update-${index}`);
-    updateButton.addEventListener("click", (e) => {
+    updateButton.addEventListener('click', (e) => {
       const updatedStatus = !myLibrary[index].status;
       myLibrary[index].status = updatedStatus;
       loadLibary();
@@ -68,7 +95,7 @@ function bookCreator(bookData, index) {
           <div class="d-flex justify-content-between align-items-center">
             <h5 class="card-title">${bookData.title}</h5>
             <i class="icon-check-sign text-success ${
-              bookData.status === !true ? "d-none" : "d-block"
+              bookData.status === !true ? 'd-none' : 'd-block'
             }"></i>
           </div>
           <h6 class="card-subtitle mb-2 text-muted">${bookData.author}</h6>
@@ -77,9 +104,9 @@ function bookCreator(bookData, index) {
           } pages</span></p>
           <button class="btn btn-danger" id="remove-${index}">Remove</button>
           <button class="btn ${
-            bookData.status === !true ? "btn-success" : "btn-secondary"
+            bookData.status === !true ? 'btn-success' : 'btn-secondary'
           }" id="update-${index}">${
-    bookData.status === !true ? "Finish" : "Unfinish"
+    bookData.status === !true ? 'Finish' : 'Unfinish'
   }</button>
         </div>
       </div>
